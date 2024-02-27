9ae0903c1d7ebbeef1081770516fb2cb6e418742
maximemateorpal <thevideofytball@gmail.com>
2024-02-27 09:22:49 +0300

    Добавьте файлы проекта.

---
+++ Bilet11project/Program.cs
@@ -0,0 +1,11 @@
+﻿// bilet 11 egor maximenko
+// напиши программу на языке с# для вычисления значения выражения y = (x - 10) / (x * 10)
+{ 
+        Console.WriteLine("Vvedite znachenie x:");
+        double x = Convert.ToDouble(Console.ReadLine());
+
+        double y = (x - 10) / (x * 10);
+
+        Console.WriteLine("Znachenie y: " + y);
+
+}
